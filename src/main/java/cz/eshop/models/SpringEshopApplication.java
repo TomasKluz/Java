b3f62b8e6146cb983305cfc94dea00d6e1934e3e
Tomas <kluz.tomas@gmail.com>
2023-09-17 16:20:56 +0200

    Create new Eshop project.

---
+++ src/main/java/cz/eshop/models/SpringEshopApplication.java
@@ -0,0 +1,10 @@
+package cz.eshop.models;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringEshopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEshopApplication.class, args);
+
+    }
+}
