4dec73e7b8480dc0e6a0097312de4c546ddd4af3
Tomas <kluz.tomas@gmail.com>
2023-09-17 20:04:22 +0200

    Vytvořena hlavní stránka a stránka about.

--- src/main/java/cz/eshop/SpringEshopApplication.java
+++ src/main/java/cz/eshop/SpringEshopApplication.java
@@ -1,6 +1,9 @@
-package cz.eshop.models;
+package cz.eshop;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 
 public class SpringEshopApplication {
     public static void main(String[] args) {
---
+++ src/main/java/cz/eshop/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package cz.eshop.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class HomeController {
+    @GetMapping("/")
+    public String renderIndex(){
+        return "pages/home/index";
+    }
+    @GetMapping("/about")
+    public  String renderAbout(){
+        return "pages/home/about";
+    }
+}
